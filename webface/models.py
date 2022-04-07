dc7982092d2b908ea1175175608883a6ede0a210
kle38146 <kle38146@spseol.cz>
2022-04-07 14:06:25 +0200

    nastenka seznam

--- webface/models.py
+++ webface/models.py
@@ -1,14 +1,16 @@
-"""
 # from datetime import datetime
-from pony.orm import PrimaryKey, Required, Optional, Database, Set
+from pony.orm import PrimaryKey, Required, Optional, Database  # , Set
 
 
 db = Database()
 db.bind(provider="sqlite", filename="./database.sqlite", create_db=True)
 
-class User(db.Entity):
-    nick = PrimaryKey(str)
-    passwd = Required(str)
+
+class Uzivatel(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    login = Required(str)
+    password = Required(str)
+    email = Optional(str)
+
 
 db.generate_mapping(create_tables=True)
-"""
--- webface/routes.py
+++ webface/routes.py
@@ -5,8 +5,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 import sqlite3
 
-dbfile= 'databaze.sqlite'
+# conn = sqlite3.connect("databaza.sqlite", isolation_level=None)
+dbfile = "databaza.sqlite"
 
+# from werkzeug.security import check_password_hash
 
 slova = ("Super", "Perfekt", "Úža", "Flask")
 
@@ -25,31 +27,37 @@ def prihlasit(function):
 @app.route("/", methods=["GET"])
 def index():
     if "nick" in session:
-        return render_template("base.html.j2")
+        with sqlite3.connect(dbfile) as conn:
+            tabulka = conn.execute("SELECT nick, text, id FROM prispevek")
+
+
+
+        return render_template("base.html.j2", tabulka=tabulka)
     else:
-        flash("Pro vstup na nástěnku je potřeba je přihlásit","error")
+        flash("Musíš se přihlásit!!!")
         return redirect(url_for("login"))
-
-
 
 @app.route("/login/")
 def login():
     return render_template("login.html.j2")
 
 
-@app.route("/login/", methods=['POST'])
+@app.route("/login/", methods=["POST"])
 def login_post():
-    nick=request.form.get("nick")
-    passwd=request.form.get("passwd")
+    nick = request.form.get("nick")
+    passwd = request.form.get("passwd")
     if nick and passwd:
         with sqlite3.connect(dbfile) as conn:
-            tabulka = list(conn.execute("SELECT passwd FROM uzivatel WHERE nick=?", [nick]))
+            tabulka = tuple(
+                conn.execute("SELECT passwd FROM uzivatel WHERE nick=?", [nick])
+            )
         if tabulka and check_password_hash(tabulka[0][0], passwd):
-            flash("Ano", "success")
+            flash("Anooooo")
             session["nick"] = nick
         else:
-            flash("Ne", "error")
+            flash("Neeeee")
     return redirect(url_for("index"))
+
 
 @app.route("/logout/")
 def logout():
@@ -57,28 +65,41 @@ def logout():
     return redirect(url_for("index"))
 
 
-@app.route("/add/")
-def add():
-    return render_template("add.html.j2")
-    nick=request.form.get('nick')
-    passwd=request.fomr.get('passwd')
+
+@app.route("/registrate/")
+def registrate():
+    return render_template("registrate.html.j2", slova=slova)
 
 
-@app.route("/add/", methods=['POST'])
-def add_post():
-    nick=request.form.get('nick')
-    passwd1=request.form.get('passwd1')
-    passwd2=request.form.get('passwd2')
+@app.route("/registrate/", methods=["POST"])
+def registrate_post():
+    nick = request.form.get("nick")
+    passwd1 = request.form.get("passwd1")
+    passwd2 = request.form.get("passwd2")
     if nick and passwd1 and passwd2 == passwd1:
         hashpasswd = generate_password_hash(passwd1)
         with sqlite3.connect(dbfile) as conn:
             try:
-                conn.execute('INSERT INTO uzivatel (nick, passwd) VALUES (?,?)', [nick, hashpasswd])
-                flash("Uživatel vytvořen.", "success")
-                session["nick"] = nick
+                conn.execute(
+                    "INSERT INTO uzivatel (nick, passwd) VALUES (?,?)",
+                    [nick, hashpasswd],
+                )
+                flash("Uživatel vytvořen.")
             except sqlite3.IntegrityError:
-                flash("Uživatel již existuje", "error")
+                flash("Uživatel již existuje!")
     else:
-        flash("Chyba: je nutné zadat jméno a dvakrát stejné heslo", "error")
-        return redirect(url_for("add"))
+        flash("Chyba: je nutné zadat jméno a dvakrát stejné heslo.")
+        return redirect(url_for("registrate"))
     return redirect(url_for("index"))
+
+
+@app.route("/insert/", methods=["POST"])
+def insert():
+    if "nick" in session:
+        prispevek = request.form.get("prispevek")
+        with sqlite3.connect(dbfile) as conn:
+            conn.execute("INSERT INTO prispevek (text, nick) VALUES (?, ?)", [prispevek, session["nick"]])
+
+        return redirect(url_for("index"))
+    else:
+        return abort(403)
